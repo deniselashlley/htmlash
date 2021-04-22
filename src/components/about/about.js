57542d2c3cbef483770111295128b85c050fa02a
Denise Lashlley <htmlash@gmail.com>
2021-04-22 12:30:37 +0100

    update styles

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,10 +4,10 @@ import { pxToRem } from '../../helper'
 import SectionTitle from '../section-title'
 
 const IntroductionText = styled.div`
-  line-height: 1.7;
-  font-size: ${pxToRem(18)};
+  line-height: 2.5;
+  font-size: ${pxToRem(20)};
   margin: 0 auto;
-  max-width: ${pxToRem(700)};
+  max-width: ${pxToRem(800)};
 
   p {
     margin-bottom:  ${pxToRem(15)};
@@ -17,7 +17,7 @@ const IntroductionText = styled.div`
   ul {
     display: flex;
     flex-flow: wrap;
-    justify-content: space-evenly;
+    justify-content: center;
     
     li {
       padding: 0 ${pxToRem(8)}
@@ -30,7 +30,7 @@ const About = () => {
     <section>
       <SectionTitle>About Me</SectionTitle>
       <IntroductionText>
-      <p>As a web professional based in London, I have been building quality websites for more than 8 years.<br/> 
+      <p>As a web professional based in London, <br/>I have been building quality websites for more than 8 years. 
         I specialise in HTML, CSS, JavaScript and other associated technologies</p>
       </IntroductionText>
       <SectionTitle>KEY SKILLS & EXPERTISE</SectionTitle>

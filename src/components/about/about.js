cdaf39f48fb272b6a57c6127f85d2fbec1c2b13c
Denise Lashlley <htmlash@gmail.com>
2021-04-22 12:20:35 +0100

    update about me text

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -30,22 +30,20 @@ const About = () => {
     <section>
       <SectionTitle>About Me</SectionTitle>
       <IntroductionText>
-      <p>As a freelancer web professional, based in London, UK. <br/>I have over 10 years of industry experience.</p><p>I specialise mainly in CSS, Javascript; 
-        and producing high quality, modern and web standard coded front end applications.</p>
-        {/* As a freelancer web professional, based in London, UK. <br />I
-        specialise in producing high quality, modern and web standard, front-end
-        development. */}
+      <p>As a web professional based in London, I have been building quality websites for more than 8 years.<br/> 
+        I specialise in HTML, CSS, JavaScript and other associated technologies</p>
       </IntroductionText>
       <SectionTitle>KEY SKILLS & EXPERTISE</SectionTitle>
       <IntroductionText>
         <ul>
-          <li>Javascript</li>
-          <li>ES6</li>
-          <li>Typescript</li>
           <li>HTML5</li>
           <li>CSS3</li>
-          <li> SASS</li>
+          <li>SASS</li>
+          <li>Javascript</li>
           <li>React</li>
+          <li>Redux</li>
+          <li>ES6</li>
+          <li>Typescript</li>
           <li>React Testing Library</li>
           <li>GATSBY</li>
           <li>GIT</li>
